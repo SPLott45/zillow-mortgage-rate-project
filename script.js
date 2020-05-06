9d9e01bb29e418b6c306251e3cee80b496390001
Stuart Lott <slott@prosper.com>
2020-05-06 01:07:19 -0700

    CSS style changes

---
+++ script.js
@@ -0,0 +1,18 @@
+//DOM Webpage Section Design 
+    //Save <body> to a variable
+
+    let bodyEl = document.querySelector('body');
+    //Save <header> element to a variable
+    let headerEl = document.querySelector('header');
+    //Save <main> element to a variable
+    let mainEl = document.querySelector('main'); 
+    //Save <footer> element to a variable
+    let footerEl = document.querySelector('footer');
+//DOM Webpage Font Design
+    //Save <h1> text to a variable 
+    let h1Text = document.querySelector('h1');
+    //Save <h2> text to a variable 
+    let h2Text = document.querySelector('h2');
+    //Save <h3> text to a varialbe 
+    let h3Text = document.querySelector('h3');
+
